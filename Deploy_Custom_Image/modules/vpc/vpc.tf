6d97fc52e5591414cf6d20bcc7abdefd01ccad58
Ikenna <okougwu@gmail.com>
2023-09-04 15:23:38 +0100

    update

--- Deploy_Custom_Image/modules/vpc/vpc.tf
+++ Deploy_Custom_Image/modules/vpc/vpc.tf
@@ -9,7 +9,8 @@ module "nkwo-vpc" {
   azs             = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  #map_public_ip_on_launch = module.map_public_ip_on_launch
+  map_public_ip_on_launch = module.map_public_ip_on_launch
+  
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
@@ -34,5 +35,4 @@ output "private_subnets" {
 output "public_subnets" {
   description = "List of IDs of public subnets"
   value       = module.nkwo-vpc.public_subnets
-  map_public_ip_on_launch = var.map_public_ip_on_launch
 }
