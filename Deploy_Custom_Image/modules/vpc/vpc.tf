cbdc3d84b00be1bff4635ce9352e0db52220ac07
Ikenna <okougwu@gmail.com>
2023-09-04 15:00:48 +0100

    update

--- Deploy_Custom_Image/modules/vpc/vpc.tf
+++ Deploy_Custom_Image/modules/vpc/vpc.tf
@@ -1,7 +1,7 @@
 #Custom VPC for my Project 
 module "nkwo-vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"
+  #version = "5.1.1"
 
   name = "vpc-${var.ENVIRONMENT}"
   cidr = "10.0.0.0/16"
@@ -9,7 +9,7 @@ module "nkwo-vpc" {
   azs             = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  map_public_ip_on_launch = module.map_public_ip_on_launch
+  #map_public_ip_on_launch = module.map_public_ip_on_launch
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
