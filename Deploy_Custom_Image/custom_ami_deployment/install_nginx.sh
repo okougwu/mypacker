5f37c7e6ffd0c259d1580ccb07b37d51db3c83f9
Ikenna <okougwu@gmail.com>
2023-09-04 12:14:53 +0100

    update

--- Deploy_Custom_Image/custom_ami_deployment/install_nginx.sh
+++ Deploy_Custom_Image/custom_ami_deployment/install_nginx.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 apt-get update
 apt-get install -y nginx docker.io
-
-#           "execute_command": "{{ .vars }} sudo -E sh '{{ .path }}'", # means I am taking all the variables and setting all the variables in the environment variable or in the path of the AMI
